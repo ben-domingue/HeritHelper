0b01726bb3ecb50487bbaf0557239faaf37430b6
Ben Domingue <ben.domingue@gmail.com>
2013-10-01 20:55:15 -0500

    tidy first draft

--- R/herit_univar.R
+++ R/herit_univar.R
@@ -1,12 +1,42 @@
 herit_univar<-function(df, #data frame with all the variables.
-                       pheno,
+                       pheno, #name of outcome
+                       grm="/tmp/hrs_tmp", #default matches make_grm default.
                        id.cols=c("family","subjectID"), #pass a character vector with their names
-                       qcovar=NULL,
-                       covar=NULL,
-                       cutoff=0.025
+                       qcovar=NULL, #names of quantitative covariates
+                       covar=NULL, #names of dichotomous covariates
+                       cutoff=0.025,
+                       out.name="/tmp/hrs_tmp",
+                       np=10
                        )
 {
-  #gcta64 --reml --reml-est-fix --grm hrs --grm-cutoff 0.025  --pheno hrs_graduate.txt --qcovar hrs_covars.txt  --out hrs_graduate_PCs --thread-num 20                         
+  df[,c(id.cols,pheno)]->pheno
+  paste0(out.name,".pheno")->fn
+  write.table(pheno,file=fn,quote=FALSE,row.names=FALSE,col.names=FALSE)
+  paste("--pheno ",fn)->pheno.txt
+  if (!is.null(qcovar)) {
+    df[,c(id.cols,qcovar)]->qcovar
+    fn<-paste0(out.name,".qcovar")
+    write.table(qcovar,file=fn,quote=FALSE,row.names=FALSE,col.names=FALSE)
+    paste("--qcovar ",fn)->qcovar.txt
+  } else ""->qcovar.txt
+  if (!is.null(covar)) {
+    df[,c(id.cols,covar)]->covar
+    paste0(out.name,".covar")->fn
+    write.table(covar,file=fn,quote=FALSE,row.names=FALSE,col.names=FALSE)
+    paste("--covar ",fn)->covar.txt
+  } else ""->covar.txt
+  ifelse(""==covar.txt & ""==qcovar.txt,"","--reml-est-fix")->fe.txt
+  paste("gcta64 --reml --grm ",grm," --grm-cutoff",cutoff,pheno.txt,qcovar.txt,covar.txt,fe.txt,"--out",out.name,"--thread-num",np)->cmd
+  system(cmd,intern=TRUE)->txt
+  #
+  tr<-list()
+  grep("V(G)/Vp",txt,fixed=TRUE)->index
+  tr$h2<-strsplit(txt[index],"\t")[[1]][2:3]
+  #
+  grep("Estimatesof fixed effects:",txt)->i1
+  grep("Summary result of REML analysis has been saved in the file",txt)->i2
+  if (length(i1)>0 & length(i2)>0) txt[i1:(i2-1)]->tr$fe
+  #
+  tr$all<-txt
+  tr
 }
-
-
--- README.md
+++ README.md
@@ -1,15 +1,33 @@
 this is meant to make some of the work for the SSM paper easier.
 
 @@@begin code
+
 library(HeritHelper)
 
 load("/hrsshare/hrs_linked.Rdata")
+
 x[x$raracem=="1.white/caucasian",]->x
+
 x[x$rahispan=="0. not hispanic",]->x
+
 x[x$rabyear>=1930 & x$rabyear<1951,]->x
 
-x[,c("family","subjectID")]->ids
-make_grm(ids,np=20)
+read.table("/hrsshare/cleaned/hrs_mds.mds",header=TRUE)->mds
+
+NULL->mds$SOL
+
+names(mds)[1:2]<-c("family","subjectID")
+
+merge(x,mds)->x
+
+
+make_grm(ids=x[,c("family","subjectID")],np=20)
+
+herit_univar(x,pheno="r8bmi",np=20)->h1
+
+herit_univar(x,pheno="r8bmi",qcovar=c("C1","C2"),np=20)->h2
+
+herit_univar(x,pheno="r8bmi",qcovar=c("raedyrs","C1","C2"),np=20)->h3
 
 @@@end code
 
