69934fdb88169baa5f46fc1a33e1f94344e5d728
Ben Domingue <ben.domingue@gmail.com>
2013-10-01 20:17:49 -0500

    moving fwd.

--- R/herit_univar.R
+++ R/herit_univar.R
@@ -1,3 +1,12 @@
-herit_univar<-function(x) {
-  NULL
+herit_univar<-function(df, #data frame with all the variables.
+                       pheno,
+                       id.cols=c("family","subjectID"), #pass a character vector with their names
+                       qcovar=NULL,
+                       covar=NULL,
+                       cutoff=0.025
+                       )
+{
+  #gcta64 --reml --reml-est-fix --grm hrs --grm-cutoff 0.025  --pheno hrs_graduate.txt --qcovar hrs_covars.txt  --out hrs_graduate_PCs --thread-num 20                         
 }
+
+
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 this is meant to make some of the work for the SSM paper easier.
+
+@@@begin code
+library(HeritHelper)
+
+load("/hrsshare/hrs_linked.Rdata")
+x[x$raracem=="1.white/caucasian",]->x
+x[x$rahispan=="0. not hispanic",]->x
+x[x$rabyear>=1930 & x$rabyear<1951,]->x
+
+x[,c("family","subjectID")]->ids
+make_grm(ids,np=20)
+
+@@@end code
+
