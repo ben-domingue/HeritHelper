8fb18bc873d6a03aecc39acb108326c35923a291
Ben Domingue <ben.domingue@gmail.com>
2014-06-02 17:03:41 -0500

    updating make_grm to make sure it does not produce binary files

--- R/make_grm.R
+++ R/make_grm.R
@@ -6,7 +6,7 @@ make_grm<-function(ids, #this is a nx2 (family, individual) matrix of ids that y
                    )
 {
   write.table(ids,file="/tmp/ids_tmp.txt",quote=FALSE,row.names=FALSE,col.names=FALSE)
-  paste("gcta64 --bfile",plink.file.path," --keep /tmp/ids_tmp.txt --autosome --maf 0.01 --make-grm --out ",out.name," --thread-num",np)->cmd
+  paste("gcta64 --bfile",plink.file.path," --keep /tmp/ids_tmp.txt --autosome --maf 0.01 --make-grm-gz --out ",out.name," --thread-num",np)->cmd
   if (!is.null(extra.txt)) paste(cmd,extra.txt)->cmd
   system(cmd)
 }
